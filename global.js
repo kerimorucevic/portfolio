d07f03d6652e4998158545f4410bb6bbbde90cb8
kerimorucevic <166185519+kerimorucevic@users.noreply.github.com>
2025-10-18 15:23:05 -0700

    new js

--- global.js
+++ global.js
@@ -3,3 +3,13 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+const $$ = (selector) => Array.from(document.querySelectorAll(selector));
+let navLinks = $$("nav a");
+
+// Step 2.2: Find the link that matches the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: Add the 'current' class if found
+currentLink?.classList.add("current");
