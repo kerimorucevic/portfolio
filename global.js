2d2c0d9050bc2bdd62e9820d797758486febcbdf
kerimorucevic <166185519+kerimorucevic@users.noreply.github.com>
2025-10-18 15:57:19 -0700

    newjs

--- global.js
+++ global.js
@@ -13,3 +13,42 @@ let currentLink = navLinks.find(
 
 // Step 2.3: Add the 'current' class if found
 currentLink?.classList.add("current");
+
+let pages = [
+    { url: '',title: 'Home' },
+    { url: 'projects/',    title: 'Projects' },
+    { url: 'about/',       title: 'About' },
+    { url: 'contact/',     title: 'Contact' },
+    { url: 'https://github.com/yourusername', title: 'GitHub' },
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+  }
+
+let a = document.createElement('a');
+a.href = url;
+a.textContent = title;
+nav.append(a);
+
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+
+
+a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+); 
