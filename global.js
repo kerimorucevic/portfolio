95fd4afecccf9ff2610dc4399620375935a81730
kerimorucevic <166185519+kerimorucevic@users.noreply.github.com>
2025-11-01 22:17:40 -0700

    pu

--- global.js
+++ global.js
@@ -61,6 +61,7 @@ const select = document.querySelector('.color-scheme select');
 
 select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
+
     document.documentElement.style.setProperty('color-scheme', event.target.value);
     if (event.target.value === 'dark') {
         document.body.style.backgroundColor = 'black';
