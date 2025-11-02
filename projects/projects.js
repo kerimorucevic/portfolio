399ee5f70d189007c4506bc07da17761a67c46b6
kerimorucevic <166185519+kerimorucevic@users.noreply.github.com>
2025-11-01 23:44:12 -0700

    proj

--- projects/projects.js
+++ projects/projects.js
@@ -15,13 +15,20 @@ const projectsContainer = document.querySelector('.projects');
 const searchInput = document.querySelector('.searchBar');
 const svg = d3.select('#projects-pie-plot');
 const legend = d3.select('.legend');
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+
 
 if (!Array.isArray(projects)) projects = [];
 
 renderProjects(projects, projectsContainer, 'h2');
 
 
-const arcGenerator = d3.arc().innerRadius(0).outerRadius(50); 
+const arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
 const sliceGenerator = d3.pie().value((d) => d.value);        
 const colors = d3.scaleOrdinal(d3.schemeTableau10);
 
